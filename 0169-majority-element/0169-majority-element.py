4cebc08d4dc98511fe6589ef5e56d74a849de99a
Sunny Shah <86511229+sunnyshah7@users.noreply.github.com>
2025-09-05 09:45:09 +0530

    Time: 15 ms (14.12%), Space: 19.3 MB (86.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums) // 2
+        freq = {}
+        
+        for num in nums:  # iterate over elements, not indices
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+        
+        for num, count in freq.items():
+            if count > n:
+                return num
